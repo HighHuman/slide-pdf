539c686fce97f9523799db9d66ad124566067e6d
Murphy Cao <murphy.cao@avepoint.com>
2024-09-16 21:05:48 +0700

    ahihi

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import slidePdf from "./components/slide-pdf.vue";
+</script>
+
+<template>
+  <slidePdf />
+</template>
+
+<style scoped>
+#app {
+  margin: 0 !important;
+  padding: 0 !important;
+}
+</style>
---
+++ src/components/slide-pdf.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="wrap">
+    <Carousel>
+      <Slide v-for="(page, index) in imgArr" :key="index">
+        <img :src="page" :alt="`${index}`" />
+      </Slide>
+    </Carousel>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { ref, onMounted } from "vue";
+
+import { Carousel, Slide } from "vue3-carousel";
+const imgArr = ref<string[]>([]);
+
+onMounted(() => {
+  for (let i = 1; i < 232; i++) {
+    imgArr.value.push("/assets/source-" + i + ".jpg");
+  }
+});
+</script>
+<style scoped>
+.wrap {
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+.carousel__item {
+  height: 95vh;
+  width: 95vw;
+}
+.carousel,
+.carousel__viewport {
+  height: 100vh;
+  width: 100vw;
+}
+
+img {
+  object-fit: cover;
+  max-height: 98vh;
+  max-width: 98vw;
+}
+.carousel {
+  display: flex;
+  align-items: center;
+}
+</style>
